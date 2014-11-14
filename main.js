d697ddab9264e4781343e09e14e557ed9d456cfd
David Millar <dave@davegoesthedistance.com>
2014-11-13 22:00:10 -0500

    Created game configuration object.

--- main.js
+++ main.js
@@ -1,8 +1,60 @@
 var pLoc = 0,
     events = [0],
-    areaName = ["Home","Yard","Neighborhood","Alley","Parking School","Grocery Store","The Park","Recruiting Office","Fred\'s Buffet","Main St."],
-    areaStatus = ["clear","geese","locked","locked","locked","locked","locked","locked","locked","locked"],
-    areaChoices = [[],["Pick Grass"],["Explore Blue House","Explore Yellow House"],[],[],["Use Pricing Gun"],["Talk to Woman"],[],[],[]],
+    gameAreas = {
+      a0: {
+        name: "Home",
+        status: "clear"
+      },
+      a1: {
+        name: "Yard",
+        status: "geese",
+        choices: [
+          "Pick Grass"
+        ]
+      },
+      a2: {
+        name: "Hood",
+        status: "locked",
+        choices: [
+          "Explore Blue House",
+          "Explore Yellow House"
+        ]
+      },
+      a3: {
+        name: "Alley",
+        status: "locked"
+      },
+      a4: {
+        name: "Parking School",
+        status: "locked"
+      },
+      a5: {
+        name: "Grocery Store",
+        status: "locked",
+        choices: [
+          "Use Pricing Gun"
+        ]
+      },
+      a6: {
+        name: "The Park",
+        status: "locked",
+        choices: [
+          "Talk to Woman"
+        ]
+      },
+      a7: {
+        name: "Recruiting Office",
+        status: "locked"
+      },
+      a8: {
+        name: "Fred\'s Buffet",
+        status: "locked"
+      },
+      a9: {
+        name: "Main St.",
+        status: "locked"
+      }
+    },
 
     gid = function (id) {
       return document.getElementById(id);
@@ -16,17 +68,17 @@ var pLoc = 0,
 
     goLocation = function (next) {
       if (pLoc != next) {
-        gid("map" + pLoc).setAttribute("class",areaStatus[pLoc]);
+        gid("map" + pLoc).setAttribute("class",gameAreas["a" + pLoc].status);
         pLoc = next;
-        gid("map" + pLoc).setAttribute("class",areaStatus[pLoc] + " here");
-        gid("currentlocation").innerHTML = areaName[pLoc];
-        gid("action").setAttribute("class",areaStatus[pLoc]);
+        gid("map" + pLoc).setAttribute("class",gameAreas["a" + pLoc].status + " here");
+        gid("currentlocation").innerHTML = gameAreas["a" + pLoc].name;
+        gid("action").setAttribute("class",gameAreas["a" + pLoc].status);
         populateChoices(pLoc);
       }
     },
 
     setStatus = function (n,s,h) {
-      areaStatus[n] = s;
+      gameAreas["a" + n].status = s;
       if (h) {
         gid("action").setAttribute("class",s);
         s += " here";
@@ -35,17 +87,19 @@ var pLoc = 0,
     },
 
     populateChoices = function (area) {
-      texts = areaChoices[area];
+      texts = gameAreas["a" + area].choices || [];
       amount = texts.length;
       if (amount > 0) {
         gid("choicebox").innerHTML = "";
-        for (i=0; i<amount; i++) { if (texts[i] != "") {
-          var a = document.createElement("a");
-          a.setAttribute("href","#");
-          a.setAttribute("onclick","adv" + area + "_" + i + "(); return false;");
-          a.innerHTML = texts[i];
-          gid("choicebox").appendChild(a);
-        } }
+        for (i=0; i<amount; i++) {
+          if (texts[i] !== "") {
+            var a = document.createElement("a");
+            a.setAttribute("href","#");
+            a.setAttribute("onclick","adv" + area + "_" + i + "(); return false;");
+            a.innerHTML = texts[i];
+            gid("choicebox").appendChild(a);
+          }
+        }
         $("#choicebox").fadeIn("slow");
       } else {
         $("#choicebox").fadeOut("slow");
@@ -67,7 +121,7 @@ var pLoc = 0,
 
       adv1_0 = function () {
         gid("description").innerHTML = "<p>You pick some grass. Not much of an accomplishment, but you did it. You leave it there.</p>";
-        if(areaStatus[2] == "locked") {
+        if(gameAreas["a" + 2].status == "locked") {
           setStatus(2,"geese",0);
           gid("description").innerHTML += "<p>A casual glance reveals the garage door opener hidden under a growth of weeds on the ground. You hit the button and hear the door open on the other side of the house.</p>";
         }
@@ -81,15 +135,15 @@ var pLoc = 0,
 
       adv2_0 = function () {
         gid("description").innerHTML = "<p>You look over the blue house and see some plastic geese decorations in the yard.</p>";
-        if((areaChoices[2].length == 2)&&(areaStatus[3] == "locked")) {
-          areaChoices[2][2] = "Destroy Plastic Geese";
+        if((gameAreas.a2.choices.length == 2)&&(gameAreas["a" + 3].status == "locked")) {
+          gameAreas.a2.choices[2] = "Destroy Plastic Geese";
           populateChoices(2);
         }
       },
 
       adv2_1 = function () {
         gid("description").innerHTML = "<p>You look over the yellow house. Nothing to see really.</p>";
-        if((areaStatus[6] == "locked")&&(areaStatus[5] == "clear")) {
+        if((gameAreas.a6.status == "locked")&&(gameAreas["a" + 5].status == "clear")) {
           gid("description").innerHTML += "<p>The newspaper kid rides by on his bike and throws the newspaper. It hits you directly in the face with a loud \'whap\' sound. It falls open on the ground and displays the ads pages.</p><p>One \'lonely people\' ad advertises a sad soul who enjoys walks in the park and eating at buffets. Her name is Shakeesha. A job offer above it is looking for young men and women to join the army and lists a nearby recruiting office.";
         setStatus(6,"clear",0);
         setStatus(7,"geese",0);
@@ -98,7 +152,7 @@ var pLoc = 0,
 
       adv2_2 = function () {
         gid("description").innerHTML = "<p>You destroy one of the plastic geese and the real geese see you do it. Boy are they scared now! They rush off leaving this area cleared and the entrance to the alleyway open.</p>";
-        areaChoices[2].pop();
+        gameAreas.a2.choices.pop();
         populateChoices(2);
         setStatus(3,"clear",0);
         setStatus(4,"geese",0);
@@ -116,9 +170,9 @@ var pLoc = 0,
     do4 = function () {
       goLocation(4);
       gid("description").innerHTML = "<p>This is Gluck\'s Parking School. Anyone can drive, but few can truly park a vehicle right.</p>";
-      if (areaStatus[5] == "geese") {
+      if (gameAreas.a5.status == "geese") {
         gid("description").innerHTML += "<p>Gluck is wearing a shirt that says \"L2P Noob!\" which stands for \"Learn 2 Park Noob!\"</p>";
-      } else if (areaStatus[4] == "geese") {
+      } else if (gameAreas.a4.status == "geese") {
         gid("description").innerHTML += "<p>Gluck is over at the grocery store raising hell.</p>";
       } else {
         gid("description").innerHTML += "<p>Gluck is out with Shakeesha.</p>";
@@ -134,7 +188,7 @@ var pLoc = 0,
 
       adv5_0 = function () {
         gid("description").innerHTML = "<p>You play around with the pricing gun, adding false labels to random items throughout the store.</p>";
-        if (areaStatus[5] == "geese") {
+        if (gameAreas.a5.status == "geese") {
           gid("description").innerHTML += "<p>Unbeknownst to you, one item you tagged was some expensive lip gloss, and you marked it down to nearly 3% the original price. A cheerleader found it, shrieked, and called all of her friends to come take advantage of the deal.</p><p>As they all arrive in a hurry, they all cram into a row of parking spaces near the front of the lot. And they park very badly.</p><p>And even though they have scared off the geese from the lot, they\'ve attracted another force of nature.</p><p>Gluck.</p>";
           setStatus(5,"clear",1);
         }
@@ -143,7 +197,7 @@ var pLoc = 0,
     do6 = function () {
       goLocation(6);
       gid("description").innerHTML = "<p>This is the park. There are geese swimming in the pond here, but I guess that\'s OK because geese are supposed to swim in the park.</p>";
-      if (areaStatus[4] == "geese") {
+      if (gameAreas.a4.status == "geese") {
         gid("description").innerHTML += "<p>There\'s a large woman sitting on a park bench eating an entire dozen of donuts.</p>";
       }
       return false;
@@ -152,15 +206,15 @@ var pLoc = 0,
       adv6_0 = function () {
         gid("description").innerHTML = "<p>You talk to the woman. It\'s Shakeesha from the newspaper ad. She tells you about her job as a meter maid and her passion for good parking and great food. She\'s 300 pounds of curvatious fun looking for the right man.</p><p>You tell her about Gluck, the guy who runs the parking school just up the road. She sounds interested.</p><p>\"What does he look like?\" she asks.</p><p>\"Remember those guys from the earlier seasons of The X Files who called themselves \'The Lone Gunmen\'? He looks like the blonde one. Or maybe like Garth from Wayne\'s World.\" you say. She gets a sparkle in her eye that can only be described as love.</p><p>She sets off toward the parking school, the earth trembling with her every step. As she draws nearer the school, you see from a distance a large flock of geese rising up from there, no doubt afraid of being Shakeesha\'s next meal. Foie gras anyone?";
         setStatus(4,"clear",0);
-        areaChoices[8][0] = "Investigate Van";
-        areaChoices[6].pop();
+        gameAreas.a8.choices[0] = "Investigate Van";
+        gameAreas.a6.choices.pop();
         populateChoices(6);
       },
 
     do7 = function () {
       goLocation(7);
       gid("description").innerHTML = "<p>This is the army recruiting office.</p>";
-      if (areaStatus[7] == "geese") {
+      if (gameAreas.a7.status == "geese") {
         gid("description").innerHTML += "<p>The parking lot is literally filled with nearly 47 geese! The men of the recruiting office have set up a barricade of sandbags and are hiding behind them trying to figure out how to get out alive.</p>";
       }
       return false;
@@ -169,7 +223,7 @@ var pLoc = 0,
     do8 = function () {
       goLocation(8);
       gid("description").innerHTML = "<p>This is \"Fred\'s Freaking Ridiculous BBQ Buffet\". It\'s run by Fred, the father of you best friend from middle school, Sam. Fred pulled Sam out of school in the freshman year of highschool to help work at the buffet. HAHA FAILURE.</p>";
-      if (areaStatus[4] == "clear") {
+      if (gameAreas.a4.status == "clear") {
         gid("description").innerHTML += "<p>The buffet is closed for the evening, but there\'s a van still parked in the parking lot. The way it\'s rocking back and forth, you\'re surprised it\'s stayling between the lines marking the parking spaces.</p>";
       }
       return false;
